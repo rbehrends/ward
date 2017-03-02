8243f68c7ae56f5437365388827c99aa9b68eba5
Reimer Behrends <behrends@gmail.com>
2017-03-02 19:29:56 +0100

    Support -cc option to explicitly specify a C compiler.

--- ward/addguards2c.lua
+++ ward/addguards2c.lua
@@ -1,3 +1,5 @@
+require "util"
+
 function catfile(file)
   local fp = io.open(file)
   local contents = fp:read("*a")
@@ -6,12 +8,20 @@ function catfile(file)
 end
 
 ward = os.getenv("WARD")
-args = table.concat({...}, " ")
+args = table.concat(map({...}, shell_escape), " ")
 argtable = {...}
 file = argtable[#argtable]
 if string.sub(file, -2, -1) == ".c" then
   guard = os.tmpname()
   if os.execute(ward .. "/bin/cward -suggest " .. args .. ">" .. guard) == 0 then
+    io.write("/***********************************************************/\n")
+    io.write("/*                                                         */\n")
+    io.write("/*       NOTE: This file was preprocessed by Ward.         */\n")
+    io.write("/*       Do not edit it; changes will be lost.             */\n")
+    io.write("/*                                                         */\n")
+    io.write("/***********************************************************/\n")
+    io.write("\n")
+    io.flush()
     os.execute(ward .. "/bin/addguardsc " .. guard .. " " .. file)
   else
     io.write("/* ERROR: Ward failed to parse C source */\n")
